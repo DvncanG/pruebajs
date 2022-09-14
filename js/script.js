9c1ac84fef56d57f38c05daee3d29a8a0a929870
Dvncan22 <julian.duncan.stw@gmail.com>
2022-09-14 19:26:15 +0200

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let numero1=parseInt(prompt("Introduce un número"));
+let numero2=parseInt(prompt("Introduce otro número"));
+
+alert("El numero es:"+(numero1+numero2));
+document.write("el número es "+(numero1+numero2));
